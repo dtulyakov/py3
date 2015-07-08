12b0690f1cb03c1c1b08c0f7737d639053ebb1af
Денис Туляков <dtulyakov@gmail.com>
2015-07-08 18:28:11 +0300

    В ветке master
     Изменения для закрепления:
    	new file:   git_tag.py
    	deleted:    gitteg.py

---
+++ src/git_tag.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from datetime import datetime, date, time
+import subprocess
+
+date_iso = datetime.today()
+tagv = ("v" + date_iso.strftime("%Y%m%d"))
+output = subprocess.check_output(["git", "tag", tagv])
