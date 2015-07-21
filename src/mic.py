fc515a34243c67502cbc3e1343fc2db13144ddc7
Денис Туляков <dtulyakov@gmail.com>
2015-07-21 14:00:08 +0300

    В ветке master
     Изменения для закрепления:
    	new file:   src/mic.py

---
+++ src/mic.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# coding: utf-8
+'''
+Можно было шандарахнуть и так но это не наш метод
+os.system(record + ("/home/ttys/frommic/" + date_iso.strftime("%Y/%m/%d/")) + (date_iso.strftime("%Y%m%d-%H") + ".mp3"))
+
+'''
+import os
+from datetime import datetime
+from distutils.dir_util import mkpath
+
+date_iso = datetime.today()
+date_path = (date_iso.strftime("%Y/%m/%d/"))
+date_file = (date_iso.strftime("%Y%m%d-%H"))
+
+path = "/home/ttys/mic/"
+record = "arecord --buffer-time=5000000 -D plughw:0,0 -f S16_LE --use-strftime - | lame - >"
+file_mp3 = date_file + ".mp3"
+
+mkpath(path)
+os.system(record + path + date_path + file_mp3)
+#EOF
